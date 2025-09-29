7bf8af307d50ad44cd17b3704385a3ca8e10e3cb
Iris Cheung <hokyinirischeung@gmail.com>
2025-09-29 12:11:50 -0700

    started wave 2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,9 @@
+import uuid
+
 class Item:
-    pass
+    def __init__(self, item, id = None)
+        self.item = item
+        self.id = uuid.uuid4()
+    
+    def grab_item_id(self, item, id):
+        print (f"{item} is  equal id {id}")
