cae7ef2e284bbccc5fe65ee433187259e2521154
Gina Song <gina0song@gmail.com>
2025-09-29 11:45:03 -0700

    updated wave_01

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,17 @@
 class Vendor:
-    pass
+    def __init__(self,inventory = None):
+        if inventory is None:
+            inventory = []
+        self.inventory = inventory
+
+    def add(self,item_to_added):
+        self.inventory.append(item_to_added)
+        return item_to_added
+    
+    def remove(self,item_to_removed):
+        if item_to_removed not in self.inventory:
+            return False
+
+        self.inventory.remove(item_to_removed)
+        return item_to_removed
+        
