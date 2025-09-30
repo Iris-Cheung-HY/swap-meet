96c73a5dfd6428b6a0a65e81611cafade7faef91
Gina Song <gina0song@gmail.com>
2025-09-30 13:45:31 -0700

    updated wave_05

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,13 +1,31 @@
 import uuid
 
 class Item:
-    def __init__(self, id = None):
-        self.id = id
-        if id == None:
-            self.id = uuid.uuid4().int
+    def __init__(self, id=None, condition=0):
+        self.id = id if id is not None else uuid.uuid4().int
+        self.condition = float(condition) 
     
     def get_category(self):
         return "Item"
         
     def __str__(self):
         return f"An object of type {self.get_category()} with id {self.id}."
+
+
+    def condition_description(self):
+        descriptions = {
+            0: "Heavily used",
+            1: "Well-loved",
+            2: "Some signs of wear",
+            3: "Lightly used",
+            4: "Almost mint",
+            5: "Mint condition"
+        }
+
+        condition_int = round(self.condition)
+        if condition_int < 0:
+            condition_int = 0
+        elif condition_int > 5:
+            condition_int = 5
+
+        return descriptions[condition_int]
